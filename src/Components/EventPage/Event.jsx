41557fff90aba920a8872b6b1cf04981552c28c0
Projesh Basnet <projeshbasnet@gmail.com>
2024-05-23 16:01:35 +0545

    updated links

--- src/Components/EventPage/Event.jsx
+++ src/Components/EventPage/Event.jsx
@@ -5,7 +5,7 @@ const Event = () => {
     return (
         <>
         <h2 id='highlight' className='mt-40 font-oswald text-center text-6xl '>Event Highlights</h2>
-            <div className="flex justify-evenly mt-6 pt-12">
+            <div className="flex flex-wrap gap-6 justify-evenly mt-6 pt-12">
                 <Card event="Guest Lecture Session by Industry Experts" image='./images/events/Guest.png'/>
                 <Card event="36 Hour Long Rigrious Hackthon" image='./images/events/Hackathon.png'/>
                 <Card event="Panel Discussion on the buzzing Topic" image='./images/events/Panel.png'/>
--- src/Components/Fragments/ButtonsSection.jsx
+++ src/Components/Fragments/ButtonsSection.jsx
@@ -4,7 +4,7 @@ import ShinyButton from './ShinyButton'
 const ButtonsSection = () => {
   return (
     <div className='mt-7 flex flex-col  md:flex-row gap-10'>
-    <ShinyButton text='Join us on Facebook' link='#'/>
+    <ShinyButton text='Join us on Facebook' link='https://www.facebook.com/share/XVkEjV2rGSuPZQ3q/?mibextid=LQQJ4d'/>
     <ShinyButton text='Register' link='https://forms.gle/onEoempEwpePvFR49'/>
     </div>
   )
